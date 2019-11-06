35a0c34b9a0b5bac5d65cac999d3764fedefd420
Mohammed shaikh <mqshaikh8@gmail.com>
2019-11-06 15:15:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -7,3 +7,10 @@
 # users_name = gets.strip
  
 # puts say_hello(users_name)
+def help()
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
